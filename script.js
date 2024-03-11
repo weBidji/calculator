7a89d26d135c5df0a9238519abdbf1ef56fd0b3e
Bidji <159267099+weBidji@users.noreply.github.com>
2024-03-11 12:40:00 -0400

    fixed some errors in button ids

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let displayValue = '0';
 const buttons = document.querySelectorAll('.normal');
 const allClearBtn = document.getElementById('ac');
 const clearBtn = document.getElementById('c');
@@ -6,40 +7,40 @@ const displayText = document.getElementById('display');
 let currentExpression = '';
 
 
-function displayUpdate(event) {
+
+function updateDisplay(event) {
     let pressedButton = event.target;
     let pressedChar = pressedButton.textContent;
     const lastChar = displayText.innerText.slice(-1);
     const restrictedChars = ['+', '-', 'x', '/', '%', '.'];
 
-
     if (restrictedChars.includes(lastChar) && restrictedChars.includes(pressedChar)) {
-
         console.log('Invalid input: ' + pressedChar);
     } else {
-        displayText.innerText += pressedChar;
+        displayText.textContent += pressedChar;
         updateFontSize();
     }
 }
 
 buttons.forEach(button => {
-    button.addEventListener('click', displayUpdate);
+    button.addEventListener('click', updateDisplay);
 });
+
 clearBtn.addEventListener('click', clear);
 
 function clear() {
     displayText.innerText = '';
 }
 
-resultBtn.addEventListener('click', calculate);
+resultBtn.addEventListener('click', operate);
 
-function calculate(expression) {
-    expression = display.textContent;
-    expression = expression.replace(/x/g, '*');
-    let result = eval(expression);
-    display.textContent = result;
-    updateFontSize()
+
+function operate(num1, operator, num2){
+
+    n
 }
+
+
 
 function updateFontSize() {
     if (displayText.textContent.length > 11) {
@@ -48,3 +49,11 @@ function updateFontSize() {
         displayText.style.fontSize = ''; // Reset to default size
     }
 }
+
+// function calculate(expression) {
+//     expression = display.textContent;
+//     expression = expression.replace(/x/g, '*');
+//     let result = eval(expression);
+//     display.textContent = result;
+//     updateFontSize()
+// }
